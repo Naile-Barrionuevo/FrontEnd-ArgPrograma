b19eb85c0ff585bc2b5b4c67f152f63809c6a39b
Naile-Barrionuevo <86542118+Naile-Barrionuevo@users.noreply.github.com>
2022-10-18 15:46:33 -0300

    perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeadersComponent } from './components/headers/headers.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { MeComponent } from './components/me/me.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeadersComponent,
     LogoApComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    MeComponent
   ],
   imports: [
     BrowserModule,
